cb92426e1ed88c0a6adca5d926ad10d7d64718e2
Антон Величко <90686860+TonyCrafter@users.noreply.github.com>
2021-12-22 20:03:03 +0300

    Add files via upload

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,6 @@
 document.querySelector(".header__btn-open").addEventListener("click", function() {
-  document.querySelector(".nav-top").classList.add("active");
+  document.querySelector(".header__nav").classList.add("active");
 })
 document.querySelector(".nav__close").addEventListener("click", function() {
-  document.querySelector(".nav-top").classList.remove("active");
+  document.querySelector(".header__nav").classList.remove("active");
 })
